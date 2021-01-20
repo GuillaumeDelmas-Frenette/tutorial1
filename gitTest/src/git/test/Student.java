30e693a0d522181423e7330a7035a898ba03ae09
Guillaume Delmas-Frenette <guillaume.delmas-frenette@mail.mcgill.ca>
2021-01-20 10:19:55 -0500

    add constructor student

--- gitTest/src/git/test/Student.java
+++ gitTest/src/git/test/Student.java
@@ -1,5 +1,19 @@
 package git.test;
 
 public class Student {
-
+	int id;
+	int name;
+	
+	public Student (int id, int name) {
+		this.id = id;
+		this.name=name;
+	}
+	
+	public int getId() {
+		return id;
+	}
+	
+	public int getName() {
+		return name;
+	}
 }
